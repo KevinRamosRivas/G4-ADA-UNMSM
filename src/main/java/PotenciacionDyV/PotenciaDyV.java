a6c456bc39d1058626f4843372cef9a402a8d1a5
Adriana Isabel <adriana.balceda@unmsm.edu.pe>
2021-08-04 11:27:04 -0500

    subiendo potenciación de enteros con Divide y vencerás

---
+++ src/main/java/PotenciacionDyV/PotenciaDyV.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PotenciacionDyV;
+
+/**
+ *
+ * @author ramos
+ */
+ public class PotenciaDyV {
+ 
+ public static int contador = 0;
+ public static int potencia(int a,int n){
+     contador ++;
+        int r;
+        if(n==0){
+        return 1;
+        }
+        else 
+            if(n%2!=0){
+            r=potencia(a,n-1);
+            return (a*r);
+            }
+            else{
+                r=potencia(a,n/2);
+                return (r*r);
+            }
+
+    }
+
+    
+ 
+    public static int vecesEjecutado(){
+        return contador;
+    }
+
+    public static void setContador(int contador) {
+        PotenciaDyV.contador = contador;
+    }
+    
+}
+
