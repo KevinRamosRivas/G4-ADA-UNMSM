fdbf28150ff6c3ed6b5511bd5217ba2ded02f5fb
Javier <javierali.perez@unmsm.edu.pe>
2021-08-26 00:51:32 -0500

    j

--- src/main/java/AlgoritmosRetroceso/AlgoritmosBackTracking/newpackage/Backtracking.java
+++ src/main/java/AlgoritmosRetroceso/AlgoritmosBackTracking/newpackage/Backtracking.java
@@ -7,10 +7,14 @@ public class Backtracking {
     private int s[];
     private int v[];
     private int tact;
-    private int p[];
+    private int p;
     
     public Backtracking(int valores[], int suma){
         v = new int[valores.length];
         s = new int[valores.length];
+        
+        v = valores;
+        tact = 0;
+        p = suma;
     }
 }
